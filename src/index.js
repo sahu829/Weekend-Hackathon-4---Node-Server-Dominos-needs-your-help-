191007b4420013340cfe5adf018f012af2db0b7a
sahu829 <sahukrishna829@gmail.com>
2021-01-24 20:58:57 +0530

    Done
    Signed-off-by: sahu829 <sahukrishna829@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const httpServer = http.createServer(handleServer);
 function handleServer(req, res) {
 
   if(req.url==='/welcome'){
-    res.writeHead(200,{'Content-Type':'text/plane'});
+    res.writeHead(200,{'Content-Type':'text/plain'});
     res.write("Welcome to Dominos!");
      res.end();
   }else if(req.url==="/contact"){
